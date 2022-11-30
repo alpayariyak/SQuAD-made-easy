3b2e01c11874b532df544e5ae6a1cd558bd6761e
alpayariyak <aariyak@wpi.edu>
2022-11-30 23:43:28 +0000

    Added to main script

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from answer_question import answer_question
 from predict import import_data, predict, prediction_to_json
 import subprocess
 
+models_to_test = [('BERT_large_finetuned', 1), ('spanBERT', 2)]
+
 if __name__ == '__main__':
-    test_data = import_data('data/dev-v2.0.json', 'test', 0.1)  # import test data
-    predictions = predict(test_data, answer_question, 3)  # predict answers
-    prediction_to_json(predictions, 'predictions_spanBERT.json')  # save predictions to JSON
-    # subprocess.run(['python', 'evaluate-v2.0.py', 'data/dev-v2.0.json', 'predictions_spanBERT.json', '--out-file=eval_spanBERT.json'])  # evaluate predictions
-    #
+    for model_name, id in models_to_test:
+        test_data = import_data('data/dev-v2.0.json', 'test')  # import test data
+        predictions = predict(test_data, answer_question, id)  # predict answers
+        prediction_to_json(predictions, f'predictions_{model_name}.json')  # save predictions to JSON
+        subprocess.run(['python', 'evaluate-v2.0.py', 'data/dev-v2.0.json', f'predictions_{model_name}.json', f'--out-file=eval_{model_name}.json'])  # evaluate predictions
+
