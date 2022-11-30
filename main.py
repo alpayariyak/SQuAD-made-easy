0703f06b73e369e6d41bc57de12dc3e006b665cc
alpayariyak <aariyak@wpi.edu>
2022-11-30 06:34:34 -0500

    Small changes

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     test_data = import_data('data/dev-v2.0.json', 'test')  # import test data
     predictions = predict(test_data, pretrained_BERT.answer_question)  # predict answers
     prediction_to_json(predictions, 'predictions.json') # save predictions to JSON
-    subprocess.run(['python', 'evaluate-v2.0.py', 'data/dev-v2.0.json', 'predictions.json'])  # evaluate predictions
+    # subprocess.run(['python', 'evaluate-v2.0.py', 'data/dev-v2.0.json', 'predictions.json'])  # evaluate predictions
 
